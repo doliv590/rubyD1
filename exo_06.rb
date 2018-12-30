74ef3870ab53013047206d1d73f217f52c2e0973
olivierde <olivier.delabasserue@yahoo.fr>
2018-12-30 16:48:28 +0100

    introduction de la commande gets.chomp qui demande à l'utilisateur d'interagir avec le programme, et enregistre la valeur consignée par celui-ci

--- exo_06.rb
+++ exo_06.rb
@@ -1,7 +1,8 @@
 number_of_hours_per_day = 10
 number_of_days_per_week = 5
 number_of_weeks_in_THP = 11
+number_of_minutes_in_an_hour = 60
 
 puts "Travail : #{number_of_hours_per_day*number_of_days_per_week*number_of_weeks_in_THP}"
-#évidement, si je n'ai pas assigné de valeur à la variable numer_of_minutes_in_an_hour que j'utilise ensuite, cela ne fonctionne pas.
+#évidement, si je n'ai pas assigné de valeur à la variable number_of_minutes_in_an_hour que j'utilise ensuite, cela ne fonctionne pas.
 puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour*number_of_hours_per_day*number_of_days_per_week*number_of_days_per_week}"
