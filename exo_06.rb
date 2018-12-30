dca20ab1d8d0bfba0790dbdfc903160b9afe8ef6
olivierde <olivier.delabasserue@yahoo.fr>
2018-12-30 16:27:19 +0100

    déclaration de variables et jeu math avec celles-ci

--- exo_06.rb
+++ exo_06.rb
@@ -3,3 +3,5 @@ number_of_days_per_week = 5
 number_of_weeks_in_THP = 11
 
 puts "Travail : #{number_of_hours_per_day*number_of_days_per_week*number_of_weeks_in_THP}"
+#évidement, si je n'ai pas assigné de valeur à la variable numer_of_minutes_in_an_hour que j'utilise ensuite, cela ne fonctionne pas.
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour*number_of_hours_per_day*number_of_days_per_week*number_of_days_per_week}"
