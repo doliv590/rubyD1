ca40a12af1f3bd1b897c8a9fd831907f350f74c4
olivierde <olivier.delabasserue@yahoo.fr>
2018-12-30 16:04:31 +0100

    petits jeux avec les opérateurs mathématiques

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compte le nombre d'heures de travail à THP"
+puts "travail : #{10 * 5 * 11}"
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+puts "et en seconde ?"
+
+puts 10 * 5 * 11 * 60 * 60
+
+puts "est-ce que c'est vrai que 3 + 2 < 5 -7"
+
+puts 3 + 2 < 5 - 7
+
+puts "ça fait combien 3 + 2 ? #{3 + 2}"
+puts "ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "ok, c'est faux alors!"
+
+puts "c'est drôle ça, faisons-en plus :"
+
+puts "est-ce que 5 est plus grand que - 2 ? #{5 > -2}"
+puts "est-ce que 5 est suppérieur ou égal à -2 ? #{5 >= -2}"
+puts "est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
